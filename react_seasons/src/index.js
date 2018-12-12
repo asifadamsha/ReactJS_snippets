0923ce3ce5a6519a29c1edd1d678affd49f0a3b5
Asif <adamsha.asif@gmail.com>
2018-12-12 18:48:31 +0100

    react class and state intro

--- react_seasons/src/index.js
+++ react_seasons/src/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(position => {
-    console.log(position);
-  });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Hello world !</div>;
-};
+    // state initialisation
+    this.state = { latitude: null, longitude: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      error => console.log(error)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Latitude : {this.state.latitude}
+        <br />
+        Longitude : {this.state.longitude}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
