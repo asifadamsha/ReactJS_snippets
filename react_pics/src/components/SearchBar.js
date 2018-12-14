6560755f001b25233bcc02cbf1d7c3f7f3f60cea
Asif <adamsha.asif@gmail.com>
2018-12-14 18:50:03 +0100

    react_pics project init

---
+++ react_pics/src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { searchWord: "Ford mustang" };
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.searchWord);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Search image</label>
+            <input
+              type="text"
+              value={this.state.searchWord}
+              onChange={event => {
+                console.log(event.target.value);
+                this.setState({ searchWord: event.target.value });
+              }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
