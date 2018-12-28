ae45f1bfdf4667606c8d3c5c25f966576109ff54
Asif <adamsha.asif@gmail.com>
2018-12-28 18:02:34 +0100

    redux-thunk added

--- redux_blog/src/actions/index.js
+++ redux_blog/src/actions/index.js
@@ -1,5 +1,17 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
-  };
+import jsonPlaceHolder from "../API/jsonPlaceHolder";
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceHolder.get("/posts");
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response.data
+  });
+};
+
+export const fetchUsers = () => async dispatch => {
+  const response = await jsonPlaceHolder.get("/users");
+  dispatch({
+    type: "FETCH_USERS",
+    payload: response
+  });
 };
